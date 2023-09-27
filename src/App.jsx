a4a1c7d9177ae119daf163e81b6021bfa2d06d91
JMateusVieira <135655011+JMateusVieira@users.noreply.github.com>
2023-09-27 22:27:14 +0000

    cop

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import Card from '../Comp / Card/Card'
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   const item1 = {
-    nome: 'Rei Ayanami',
-    imagemUrl: 'https://i.ytimg.com/vi/WBOKz8SYQJo/maxresdefault.jpg'
+    nome: 'Rick Sanchez',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
+
+  const item2 = {
+    nome: 'Morty Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
+  const item3 = {
+    nome: 'Summer Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+
+  const itens = [item1, item2, item3]
+
   return (
     <>
-      <Card item="teste" />
-      
+      {itens.map(function (item) {
+        return <Card item={item}/>
+      })}
     </>
-    )
-  }
+  )
+}
+
 export default App
