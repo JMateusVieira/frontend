8445010b05e08b65511e116cc023a0eb6b0c424a
JMateusVieira <135655011+JMateusVieira@users.noreply.github.com>
2023-09-27 23:17:21 +0000

    mod

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import './App.css'
-import Card from './components/Card/Card'
+import Card from './components/Card/Card/Card'
 
 function App() {
   const item1 = {
     nome: 'Rick Sanchez',
-    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+    imagemUrl: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQvq1zPN95dBDQfKoqbFlN0GoneO_6spmHfLQOm0_fEIxVMSL7h'
   }
 
   const item2 = {
     nome: 'Morty Smith',
-    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+    imagemUrl: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQo6Cf5eg53CLVRki3nZOs2QcCByOCLFCrHPGef1P2eok9v7uJT'
   }
 
   const item3 = {
     nome: 'Summer Smith',
-    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+    imagemUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtgmx2TBsmYNlKj2_fqRsGCjBS8F8VbQ2Q-A&usqp=CAU'
   }
 
   const itens = [item1, item2, item3]
